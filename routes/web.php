0d71cc94c0b264ccb0a907bfd5461498be773b17
viviovilala <hauranabila2244@gmail.com>
2025-08-19 13:52:25 +0700

    save

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
-use App\Http\Controllers\LampiranController;
 use App\Http\Controllers\PendaftaranController;
 use App\Mail\TestMail;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PendaftaranController;
 
-<<<<<<< HEAD
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,40 +16,15 @@ use App\Http\Controllers\PendaftaranController;
 |
 */
 
-// Rute untuk halaman utama dan pendaftaran
-Route::get('/', function () {
-    return redirect()->route('pendaftaran.create');
-});
-
-// Route::get('/pendaftaran', [PendaftaranController::class, 'create'])->name('pendaftaran.create');
-// Route::post('/pendaftaran', [PendaftaranController::class, 'store'])->name('pendaftaran.store');
-
-
-// Rute bawaan Laravel untuk dashboard dan settings
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-=======
-// ============================
-// Halaman Utama (Landing Page)
-// ============================
+// Halaman utama dan pendaftaran
 Route::get('/', function () {
     return view('landing');
 })->name('landing');
 
-// ============================
-// Pendaftaran
-// ============================
-// Form pendaftaran
-Route::get('/pendaftaran', [PendaftaranController::class, 'index'])->name('pendaftaran');
->>>>>>> e6dc1422201d5091cf1859ee3cd60966025e80cd
+// Rute untuk pendaftaran (menggunakan resource controller)
+Route::resource('pendaftaran', PendaftaranController::class);
 
-// Proses submit pendaftaran
-Route::post('/pendaftaran', [PendaftaranController::class, 'submit'])->name('pendaftaran.submit');
-
-// ============================
-// Halaman Detail Pelatihan
-// ============================
+// Rute untuk halaman detail pelatihan
 Route::get('/pelatihan/{kompetensi}', function ($kompetensi) {
     $kompetensiList = [
         'tata-boga',
@@ -61,38 +33,39 @@ Route::get('/pelatihan/{kompetensi}', function ($kompetensi) {
         'teknik-pendingin-dan-tata-udara',
     ];
 
-<<<<<<< HEAD
-
-Route::get('/send', function () {
-    Mail::to(['23082010166@student.upnjatim.ac.id'])->send(new TestMail());
-});
-
-
-Route::get('/send', function () {
-    Mail::to(['23082010166@student.upnjatim.ac.id'])->send(new TestMail());
-});
-
-Route::resource('pendaftaran', PendaftaranController::class);
-
-route::get('1', function () {
-    return view('peserta.pendaftaran.bio-peserta');
-});
-route::get('2', function () {
-    return view('peserta.pendaftaran.bio-sekolah');
-});
-route::get('3', function () {
-    return view('peserta.pendaftaran.lampiran');
-});
-route::get('4', function () {
-    return view('peserta.pendaftaran.selesai');
-});
-// Rute untuk autentikasi (login, register, dll.)
-require __DIR__ . '/auth.php';
-=======
     if (!in_array($kompetensi, $kompetensiList)) {
         abort(404);
     }
 
     return view('detail-pelatihan', compact('kompetensi'));
 })->name('detail-pelatihan');
->>>>>>> e6dc1422201d5091cf1859ee3cd60966025e80cd
+
+// Rute untuk mengirim email (gunakan salah satu, jangan duplikasi)
+Route::get('/send', function () {
+    Mail::to(['23082010166@student.upnjatim.ac.id'])->send(new TestMail());
+});
+
+// Rute bawaan Laravel untuk dashboard dan settings
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+// Rute untuk autentikasi (login, register, dll.)
+require __DIR__ . '/auth.php';
+
+// Rute-rute ini tampaknya digunakan untuk pengujian dan dapat dihapus setelah selesai
+Route::get('1', function () {
+    return view('peserta.pendaftaran.bio-peserta');
+});
+
+Route::get('2', function () {
+    return view('peserta.pendaftaran.bio-sekolah');
+});
+
+Route::get('3', function () {
+    return view('peserta.pendaftaran.lampiran');
+});
+
+Route::get('4', function () {
+    return view('peserta.pendaftaran.selesai');
+});
