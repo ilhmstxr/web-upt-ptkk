b95b1ba537c6965a10af988e1516d1ca48e9fa29
ilhmstxr <ilhambintanghuh30332@gmail.com>
2025-08-27 13:52:51 +0700

    mod: haura

--- database/migrations/2025_08_26_200830_create_post_test_answers_table.php
+++ database/migrations/2025_08_26_200830_create_post_test_answers_table.php
@@ -12,14 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_test_answers', function (Blueprint $table) {
-    $table->id();
-    $table->foreignId('user_id')->constrained()->onDelete('cascade');
-    $table->foreignId('post_test_id')->constrained()->onDelete('cascade');
-    $table->string('answer');      // jawaban user
-    $table->boolean('is_correct'); // benar/salah
-    $table->timestamps();
-});
-
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_test_id')->constrained()->onDelete('cascade');
+            $table->string('answer');      // jawaban user
+            $table->boolean('is_correct'); // benar/salah
+            $table->timestamps();
+        });
     }
 
     /**
--- database/migrations/2025_08_26_213335_create_pre_test_answers_table.php
+++ database/migrations/2025_08_26_213335_create_pre_test_answers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post_test_answers', function (Blueprint $table) {
+        Schema::create('pre_test_answers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('post_test_id')->constrained()->onDelete('cascade');
