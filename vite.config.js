41c0e1aad1cde1298bf5d86dc0672c6b004bd12b
ilhmstxr <ilhambintanghuh30332@gmail.com>
2025-11-28 09:17:25 +0700

    sementara login admin

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
     ],
     css: {
         postcss: {
+            
             plugins: {
                 "tailwindcss/nesting": "postcss-nesting",
                 tailwindcss: {},
