0b4b697b14c14ba0da570d0250795835c3530afa
ilhmstxr <ilhambintanghuh30332@gmail.com>
2025-12-07 05:12:12 +0700

    benerin migrasi + import, fix bisa

--- database/migrations/2025_09_04_014919_create_pendaftaran_pelatihan_table.php
+++ database/migrations/2025_09_04_014919_create_pendaftaran_pelatihan_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('pelatihan_id')->constrained('pelatihan')->cascadeOnDelete();
             
             // PENTING: Uncomment ini jika Controller Anda memakai 'bidang_id'
-            $table->foreignId('bidang_id')->constrained('bidang')->cascadeOnDelete(); 
+            // $table->foreignId('bidang_id')->constrained('bidang')->cascadeOnDelete(); 
 
             $table->foreignId('bidang_pelatihan_id')->nullable()
                 ->constrained('bidang_pelatihan')
